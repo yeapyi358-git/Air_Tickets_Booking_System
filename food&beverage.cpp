1dc1cd42a21d09156a7025d7ea599726ee250eb0
aqilahhusna <Aqilahhisam0@student.usm.my>
2025-12-07 01:22:18 +0800

    in progress

---
+++ food&beverage.cpp
@@ -0,0 +1,12 @@
+//Programmer : NURUL AQILAH HUSNA BINTI MOHAMAD NOR HISAM 
+//Purpose : let user to add on food and beverage in their booking ticket
+
+#include"food&beverage.hpp"
+#include<string>
+#include<iostream>
+#include<iomanip>
+using namespace std;
+
+void showmealsmenu(){
+    
+}

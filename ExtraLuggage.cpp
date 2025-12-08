1d8686c288bdc40a1ab2eb7f36b9feae17a08fe1
aqilahhusna <Aqilahhisam0@student.usm.my>
2025-12-08 20:45:59 +0800

    in progress

---
+++ ExtraLuggage.cpp
@@ -0,0 +1,34 @@
+//Programmer : NURUL AQILAH HUSNA BINTI MOHAMAD NOR HISAM
+//Purpose : Let user to add an extra luggage if it is needed
+
+#include<iostream>
+#include<iomanip>
+using namespace std;
+
+void showLuggageMenu() {
+    cout << "\n=== EXTRA LUGGAGE OPTIONS ===\n";
+    cout << "1. 10 kg  - RM50\n";
+    cout << "2. 20 kg  - RM90\n";
+    cout << "3. 30 kg  - RM130\n";
+}
+
+double calculateExtraLuggage() {
+    int choice;
+    double fee = 0;
+
+    showLuggageMenu();
+    cout << "Select luggage weight (1-3): ";
+    cin >> choice;
+
+    switch (choice) {
+        case 1: fee = 50; break;
+        case 2: fee = 90; break;
+        case 3: fee = 130; break;
+        default:
+            cout << "Invalid choice.\n";
+            fee = 0;
+    }
+
+    return fee;
+}
+
---
+++ ExtraLuggage.hpp
@@ -0,0 +1,2 @@
+void showLuggageMenu();
+double calculateExtraLuggage();
